31e63a3a66ff97db6b9630a31738aa773a931db2
alfux <alexis.t.fuchs@gmail.com>
2024-02-06 20:39:51 +0100

    Estimation correcte

--- src/main_train.cpp
+++ src/main_train.cpp
@@ -1,18 +1,22 @@
 #include <fstream>
+#include <cmath>
 #include <Error.hpp>
 
-static void	compute(std::ifstream &data, std::string &line, double *theta, double learning_rate)
+#define EPSILON 0.000001
+
+static void	learningRate(std::string &line, double *theta, double *theta_next)
 {
-	size_t			count;
+	std::ifstream	data("./data.csv");
 	double			mileage;
 	double			price;
-	double			theta_next[2];
-	double			to_minimise;
+	double			alpha;
+	double			div;
 
-	count = 0;
-	theta_next[0] = 0;
-	theta_next[1] = 0;
-	to_minimise = 0;
+	if (!data.is_open())
+		throw (Error(2, "couldn't open file", "data.csv"));
+	alpha = 0;
+	div = 0;
+	std::getline(data, line);
 	std::getline(data, line);
 	while (!data.eof())
 	{
@@ -20,19 +24,45 @@ static void	compute(std::ifstream &data, std::string &line, double *theta, doubl
 		line.erase(0, line.find(","));
 		line.erase(0, 1);
 		price = std::stod(line);
-		theta_next[0] += (theta[0] + theta[1] * mileage) - price;
-		theta_next[1] += ((theta[0] + theta[1] * mileage) - price) * mileage;
-		to_minimise += ((theta[0] + theta[1] * mileage) - price) * ((theta[0] + theta[1] * mileage) - price);
+		alpha += (price - (theta[1] * mileage + theta[0])) * (theta_next[1] * mileage + theta_next[0]);
+		div += pow(theta_next[1] * mileage + theta_next[0], 2);
+		std::getline(data, line);
+	}
+	alpha /= div;
+	theta[0] += alpha * theta_next[0];
+	theta[1] += alpha * theta_next[1];
+}
+
+static void	compute(std::ifstream &data, std::string &line, double *theta)
+{
+	size_t			count;
+	double			mileage;
+	double			price;
+	double			diff;
+	double			theta_next[2];
+
+	count = 0;
+	theta_next[0] = 0;
+	theta_next[1] = 0;
+	std::getline(data, line);
+	while (!data.eof())
+	{
+		mileage = std::stod(line);
+		line.erase(0, line.find(","));
+		line.erase(0, 1);
+		price = std::stod(line);
+		diff = (theta[0] + theta[1] * mileage) - price;
+		theta_next[0] += diff;
+		theta_next[1] += diff * mileage;
 		++count;
 		std::getline(data, line);
 	}
-	theta[0] -= learning_rate * theta_next[0] / count;
-	theta[1] -= learning_rate * theta_next[1] / count;
-	to_minimise /= count;
-	std::cout << "to_minimise: " << to_minimise << std::endl;
+	theta_next[0] /= -count;
+	theta_next[1] /= -count;
+	learningRate(line, theta, theta_next);
 }
 
-static void	learn(double *theta, double learning_rate)
+static void	learn(double *theta)
 {
 	std::ifstream	data("./data.csv");
 	std::string		line;
@@ -40,7 +70,7 @@ static void	learn(double *theta, double learning_rate)
 	if (!data.is_open())
 		throw (Error(2, "couldn't open file", "data.csv"));
 	std::getline(data, line);
-	compute(data, line, theta, learning_rate);
+	compute(data, line, theta);
 }
 
 static void	save(double *theta)
@@ -58,20 +88,18 @@ int	main(void)
 	{
 		double	theta[2];
 		double	last_theta[2];
-		double	learning_rate;
 
 		theta[0] = 0;
 		theta[1] = 0;
-		learning_rate = 0.001;
 		do {
 			last_theta[0] = theta[0];
 			last_theta[1] = theta[1];
-			learn(theta, learning_rate);
-			std::cout << theta[0] << "," << theta[1] << std::endl;
-			learning_rate /= 1000;
-		} while (theta[0] != last_theta[0] || theta[1] != last_theta[1]);
-		std::cout << "Final computed parameters:" << std::endl << "theta0: "
-			<< theta[0] << std::endl << "theta1: " << theta[1] << std::endl;
+			learn(theta);
+			std::cout << "theta0 " << theta[0] << ", theta1 " << theta[1] << std::endl;
+		} while (sqrt(pow(theta[0] - last_theta[0], 2) + pow(theta[1] - last_theta[1], 2)) > EPSILON);
+		std::cout << "Final computed parameters:" << std::endl
+			<< "theta0: " << theta[0] << std::endl
+			<< "theta1: " << theta[1] << std::endl;
 		save(theta);
 	}
 	catch (std::exception const &e)
